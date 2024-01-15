45cb607ea37c5910c7650a362fbbd91aa7049932
Ilja Plotnikov <example@mail.com>
2024-01-15 14:27:14 +0200

    week 4

---
+++ Week6StringandArray/Program.cs
@@ -0,0 +1,12 @@
+﻿string userName = "Darth Vader";
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Lenght - 1]}. ");
+
+
+//replace
+//a - 4
+
+string updateUserName = userName.Replace('a', '4').Replace('e', '3');
+
+Console.WriteLine(updateUserName);
